237f53d2dd366904629da0e7b588342f0090ef96
David Andersson <david.andersson@alumni.chalmers.se>
2016-04-29 09:47:39 +0200

    complex types

---
+++ DemoConsole/DemoConsole/Models/Address.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DemoConsole.Models
+{
+    [ComplexType]
+    public class Address
+    {
+        public Address() { }
+
+        public string Email { get; set; }
+        public string StreetAddress { get; set; }
+
+    }
+}
---
+++ DemoConsole/DemoConsole/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using DemoConsole.Models;
+
+namespace DemoConsole
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to school");
+            SchoolContext context = new SchoolContext();
+            int count = context.Students.Count();
+            Console.WriteLine("There are " + count + " students");
+            Console.WriteLine("Program done");
+            Console.ReadLine();
+        }
+    }
+}
